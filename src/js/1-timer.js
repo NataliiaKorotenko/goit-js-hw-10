4c12fb96b71b58103f97048ec3bd3f371385c042
NataliiaKorotenko <119405955+NataliiaKorotenko@users.noreply.github.com>
2024-06-25 19:34:27 +0200

    timer fini

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,25 +1,20 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
-import iziToast from "izitoast";
-import "izitoast/dist/css/iziToast.min.css";
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 
 const dateToday = new Date();
-let timer = null;
 let timerId = null;
 let selectedDateSecond = null;
 const buttonStart = document.querySelector('button[data-start]');
 buttonStart.addEventListener('click', startTimer);
 buttonStart.setAttribute('disabled', 'disabled');
 
-let { spanDays, spanHours, spanMin, spanSec } = {
-  spanDays: document.querySelector('span[data-days]'),
-  spanHours: document.querySelector('span[data-hours]'),
-  spanMin: document.querySelector('span[data-minutes]'),
-  spanSec: document.querySelector('span[data-seconds]'),
-};
-
-
+const spanDays = document.querySelector('span[data-days]');
+const spanHours = document.querySelector('span[data-hours]');
+const spanMin = document.querySelector('span[data-minutes]');
+const spanSec = document.querySelector('span[data-seconds]');
 
 const options = {
   enableTime: true,
@@ -28,76 +23,89 @@ const options = {
   minuteIncrement: 1,
   onOpen() {
     clearInterval(timerId);
-    timer = null;
     timerId = null;
     addItem(0);
   },
-    onClose(selectedDates) {
-       selectedDateSecond = selectedDates[0].getTime();
-       checkDate(selectedDateSecond);
-    //  console.log(selectedDates[0]);
+  onClose(selectedDates) {
+    selectedDateSecond = selectedDates[0].getTime();
+    checkDate(selectedDateSecond);
   },
 };
 
-flatpickr("#datetime-picker", options);
+flatpickr('#datetime-picker', options);
 
-function checkDate(selectedDateSecond){
-    if (dateToday - selectedDateSecond < 0 ) { 
-        buttonStart.removeAttribute('disabled', 'disabled');
-    } 
-    else {
-        iziToast.failure('Please choose a date in the future');
-        buttonStart.setAttribute('disabled', 'disabled');
-    };
-};
+function checkDate(selectedDateSecond) {
+  const dateNow = new Date().getTime();
+  if (selectedDateSecond > dateNow) {
+    buttonStart.removeAttribute('disabled');
+  } else {
+    iziToast.error({
+      title: 'Error',
+      message: 'Please choose a date in the future',
+    });
+    buttonStart.setAttribute('disabled', 'disabled');
+  }
+}
 
-function startTimer(){
-    if (timer) {
-        iziToast.warning('The timer has already started!');
-        return
+function startTimer(event) {
+  event.preventDefault();
+  if (timerId) {
+    iziToast.warning({
+      title: 'Warning',
+      message: 'The timer has already started!',
+    });
+    return;
+  }
+
+  buttonStart.setAttribute('disabled', 'disabled');
+  document
+    .querySelector('#datetime-picker')
+    .setAttribute('disabled', 'disabled');
+
+  timerId = setInterval(() => {
+    const dateNow = new Date().getTime();
+    const timer = selectedDateSecond - dateNow;
+
+    if (timer <= 0) {
+      iziToast.success({
+        title: 'Success',
+        message: 'Timer is completed!',
+      });
+      clearInterval(timerId);
+      addItem(0);
+      document.querySelector('#datetime-picker').removeAttribute('disabled');
+      return;
     }
-    timerId = setInterval(() => {
-        const dateNaw = new Date();
-        timer = selectedDateSecond - dateNaw;
-        if (timer < 0) {
-           iziToast.success('Timer is completed!');
-            clearInterval(timerId);
-            return
-        }
-        addItem(timer);
-    }, 1000);  
-};
 
-function  addItem(timer) {
-    const { days, hours, minutes, seconds } = convertMs(timer);
-    spanDays.textContent = days;
-    spanHours.textContent = hours;
-    spanMin.textContent = minutes;
-    spanSec.textContent = seconds;
-};
+    addItem(timer);
+  }, 1000);
+}
+
+function addItem(timer) {
+  const { days, hours, minutes, seconds } = convertMs(timer);
+  spanDays.textContent = addLeadingZero(days);
+  spanHours.textContent = addLeadingZero(hours);
+  spanMin.textContent = addLeadingZero(minutes);
+  spanSec.textContent = addLeadingZero(seconds);
+}
 
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
-  // Remaining days
   const days = Math.floor(ms / day);
-  // Remaining hours
   const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
   const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
 }
 
-function addLeadingZero(value){
-    return String(value).padStart(2, '0');
-};
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
 
 // console.dir(spanSec)
 
